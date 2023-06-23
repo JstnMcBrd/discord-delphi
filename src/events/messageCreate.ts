3d615503da664ac0503ebdbced299f0941039fb2
Justin McBride <jstnmcbrd@outlook.com>
2023-06-23 02:20:51 -0600

    Update to new format from discord-cleverbot

---
+++ src/events/messageCreate.ts
@@ -0,0 +1,125 @@
+import type { Message, TextBasedChannel } from "discord.js";
+import fetch from "node-fetch";
+
+import { EventHandler } from "./EventHandler.js";
+import { formatPrompt } from "../utils/formatPrompt.js";
+import { doesMentionSelf, isFromSelf, isEmpty } from "../utils/messageAnalysis.js";
+import { replyWithError } from "../utils/replyWithError.js";
+import { sleep } from "../utils/sleep.js";
+import { typingSpeed } from "../parameters.js";
+import { debug, error, info } from "../logger.js";
+
+/** Called whenever the client observes a new message. */
+export const messageCreate = new EventHandler("messageCreate")
+	.setOnce(false)
+	.setExecution(async message => {
+		// TODO
+
+		// Ignore certain messages
+		if (isFromSelf(message)) {
+			return;
+		}
+		if (isEmpty(message)) {
+			return;
+		}
+		if (!doesMentionSelf(message)) {
+			return;
+		}
+
+		try {
+			// Format the prompt
+			const prompt = formatPrompt(message);
+
+			// Generate response
+			const response = await fetchDelphiResponse(prompt);
+
+			logExchange(message.channel, prompt, response);
+
+			// Pause to pretend to "type" the message
+			const timeTypeSec = response.length / typingSpeed;
+			await message.channel.sendTyping();
+			await sleep(timeTypeSec * 1000);
+
+			// Send the message
+			await sendOrReply(message, response);
+		}
+		catch (err) {
+			error(err);
+
+			// If unknown error, respond with error message
+			await replyWithError(message, err);
+		}
+
+	});
+
+/**
+ * Formats the request URL for `fetchJSON` and parses the JSON for the answer text.
+ *
+ * @param input // TODO
+ * @returns // TODO
+ */
+async function fetchDelphiResponse (input: string) {
+	if (input === "") {
+		input = " ";
+	}
+	const requestURL = `https://mosaic-api-frontdoor.apps.allenai.org/predict?action1=${encodeURIComponent(input)}`;
+	// TODO make type-safe
+	const json = await fetchJSON(requestURL) as { answer: { text: string } };
+	return json.answer.text;
+}
+
+/**
+ * Fetches a JSON from a URL.
+ *
+ * @param requestURL // TODO
+ * @returns // TODO
+ */
+async function fetchJSON (requestURL: string): Promise<unknown> {
+	info(`\tFetching from URL: ${requestURL}`);
+	const response = await fetch(requestURL);
+	if (response.status !== 200 || response.statusText !== "OK") {
+		const err = new Error(`${response.status} ${response.statusText}`);
+		err.name = "HTTPError";
+		throw err;
+	}
+
+	const json = await response.json();
+	info(`\tFetched JSON: ${JSON.stringify(json)}`);
+	return json;
+}
+
+/**
+ * Logs the current exchange.
+ */
+function logExchange (channel: TextBasedChannel, prompt: string, response: string): void {
+	info("Generated response");
+	debug(`\tChannel: ${
+		channel.isDMBased()
+			? `@${channel.recipient?.username ?? "unknown user"}`
+			: `#${channel.name}`
+	} (${channel.id})`);
+	debug(`\t──> ${prompt}`);
+	debug(`\t<── ${response}`);
+}
+
+/**
+ * Sends the given response, either using the channel's `send` method or the message's `reply`
+ * method, depending on whether the message is the latest message in the channel.
+ *
+ * @param message The message to reply to
+ * @param response The response to send
+ * @returns The response as a `Message` object
+ */
+async function sendOrReply (message: Message, response: string): Promise<Message> {
+	// Use reply if message is not the latest message
+	return isLatestMessage(message)
+		? message.channel.send(response)
+		: message.reply(response);
+}
+
+/**
+ * @returns Whether the given message is the latest message in its channel
+ */
+function isLatestMessage (message: Message): boolean {
+	return message.channel.lastMessageId === message.id;
+}
