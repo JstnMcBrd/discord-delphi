8c4d68fd8de1ccebc1c80fa7e41a66278383e2e4
Justin McBride <jstnmcbrd@outlook.com>
2026-04-21 03:32:21 -0600

    Abort on `clientReady` errors to avoid bad state (#167)

--- src/events/clientReady.ts
+++ src/events/clientReady.ts
@@ -1,13 +1,24 @@
+import { exit } from 'node:process';
+
 import { Events } from 'discord.js';
 
 import { EventHandler } from './EventHandler.ts';
-import { debug } from '../logger.ts';
 import { syncCommands } from '../commands/index.ts';
+import { debug, error, info } from '../logger.ts';
 
 /** Called once the client successfully logs in. */
 export const clientReady = new EventHandler(Events.ClientReady)
 	.setOnce(true)
 	.setExecution(async (client) => {
-		debug(`\tUser: ${client.user.username} (${client.user.id})`);
-		await syncCommands(client);
+		try {
+			debug(`\tUser: ${client.user.username} (${client.user.id})`);
+			await syncCommands(client);
+		}
+		catch (err) {
+			error(err);
+
+			// If the clientReady event fails, the bot is not properly initialized and must abort
+			info('Aborting...');
+			exit(1);
+		}
 	});
